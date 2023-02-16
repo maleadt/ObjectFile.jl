68274d0c82e101ad6d13e2d21d8ada46974b3349
Tim Besard <tim.besard@gmail.com>
2023-02-16 14:26:45 +0100

    Add rudimentary support for fat MachO files

--- src/MachO/MachO.jl
+++ src/MachO/MachO.jl
@@ -22,6 +22,7 @@ include("constants.jl")
 
 # Start to bring in concrete types, in the order they're needed
 include("MachOHeader.jl")
+include("MachOFat.jl")
 include("MachOHandle.jl")
 include("MachOLoadCmd.jl")
 include("MachOSegment.jl")
@@ -30,9 +31,6 @@ include("MachODynamicLink.jl")
 include("MachOStrTab.jl")
 include("MachOSymbol.jl")
 
-# We do not yet support Fat (Universal) MachO binaries, as I have yet to come
-# up with a nice abstraction over them that fits in well with COFF/ELF.
-include("MachOFat.jl")
-
 
 end # module MachO
+
